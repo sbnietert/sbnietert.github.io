359ca693f2d1d6cf1d8b0b33fce55838b71fcc3c
Sloan Nietert <sloan.nietert@gmail.com>
2025-09-01 15:42:57 +0200

    removed _https

--- _https/default.php
+++
@@ -1,7 +0,0 @@
-<?php
-if( (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 80 ) {
-    header('Location: https:///www.cs.cornell.edu/~nietert/');
-}
-include('content.html');
-exit;
-?>
