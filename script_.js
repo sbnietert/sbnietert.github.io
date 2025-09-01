e10dbfc9a22617d9e87a851f3fe187450a5094d6
Sloan Nietert <sloan.nietert@gmail.com>
2025-09-01 16:36:57 +0200

    more tweaks

--- script_.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.bib-copy').forEach(button => {
-  button.addEventListener('click', () => {
-    const pre = button.nextElementSibling; // get the <pre> block
-    navigator.clipboard.writeText(pre.textContent)
-      .then(() => { button.textContent = 'Copied!'; setTimeout(() => button.textContent = 'Copy', 1000); })
-      .catch(err => console.error('Copy failed', err));
-  });
-});
